9e2aa4a2798e2fbf1f60644c37081e90560ee1a3
chentao <827345030@>
2017-02-16 00:29:31 +0800

    添加自定义view

---
+++ app/src/main/kotlin/com/example/administrator/kotlinapp/SettingView.kt
@@ -0,0 +1,17 @@
+package com.example.administrator.kotlinapp
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.TextView
+
+/**
+ * Created by chentao on 2017/2/16.
+ * 设置view
+ */
+class SettingView : TextView {
+    constructor(context: Context, attributeSet: AttributeSet, defStyle : Int) :super(context, attributeSet, defStyle) {
+
+    }
+
+
+}
